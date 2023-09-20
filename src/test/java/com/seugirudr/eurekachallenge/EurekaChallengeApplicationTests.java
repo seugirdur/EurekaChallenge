68ceacf9b1c5e76b6e325b390b978a8dc05c9958
Gabriel Rudrigues Lima <gabriel.lima137170@gmail.com>
2023-09-20 17:08:23 -0300

    Initial commit

---
+++ src/test/java/com/seugirudr/eurekachallenge/EurekaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seugirudr.eurekachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
