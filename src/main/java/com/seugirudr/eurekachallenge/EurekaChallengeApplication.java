7cd472b3b22a70c4bc51656df0939f08dd3861a9
Gabriel Rudrigues Lima <gabriel.lima137170@gmail.com>
2023-09-21 00:46:35 -0300

    termino da configuracao do jwt

--- src/main/java/com/seugirudr/eurekachallenge/EurekaChallengeApplication.java
+++ src/main/java/com/seugirudr/eurekachallenge/EurekaChallengeApplication.java
@@ -2,12 +2,14 @@ package com.seugirudr.eurekachallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaChallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaChallengeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaChallengeApplication.class, args);
+    }
 
 }
